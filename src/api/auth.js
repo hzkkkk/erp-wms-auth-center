1241f2b524d54661380a036e25cea7f624ad5d3b
Bug Hunter <2185219021@qq.com>
2021-03-26 20:11:03 +0800

    BUG-Hunter feat login : add validation

--- src/api/auth.js
+++ src/api/auth.js
@@ -23,3 +23,32 @@ export function login(data) {
         params: data
     })
 }
+
+// 获取协议内容
+export function getXieyi() {
+    return request({
+        url: `${window.location.href}/xieyi.html`, // 访问到的是 public/xieyi.html
+        method: 'get'
+    })
+}
+
+
+// 查询用户名是否已被注册
+export function getUserByUsername(username) {
+    return request({
+        url: `/system/api/user/username/${username}`,
+        method: 'get'
+    })
+}
+
+
+// 提交注册数据
+export function register(data) {
+    return request({
+        headers,
+        auth,
+        url: `/system/api/user/register`,
+        method: 'post',
+        params: data
+    })
+}
